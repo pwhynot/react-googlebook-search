d8d1bdbd6ceab05d58d70014954c0481fcfec9c9
unknown <pwhynotfilms@gmail.com>
2019-07-18 13:39:56 -0500

    components added

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { Component } from "react";
+import "./App.css";
+import Navbar from "../components/nav/navbar";
+import Header from "../components/header/header";
+import Search from "../components/search/serach";
+import Result from "../components/results/results";
 
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        
-      </header>
-    </div>
-  );
+
+class App extends Component {
+  state = {
+    query: ""
+  }
+
+  render() {
+    return (
+      <div className="App">
+       <Navbar />
+       <Header />
+       <Search searchBox={this.state.query} />
+       <div className="resultBox">
+       <Result />
+       </div>
+      </div>
+    );
+  }
 }
 
 export default App;
